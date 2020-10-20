dd2996ba627313f9eeed160c4e754ecfc8b6d9c4
zzzfisherzzz <zzzfisherzzz@gmail.com>
2020-10-20 07:01:19 +0300

    upload files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import * as serviceWorker from './serviceWorker';
+import { rootReducer } from './redux/rootReducer';
+import { forbiddenWordsMiddleware } from './redux/middleware';
+import { sagaWatcher } from './redux/sagas'
+
+const saga = createSagaMiddleware()
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk, forbiddenWordsMiddleware, saga
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+saga.run(sagaWatcher)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
